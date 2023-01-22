d8765095646764bc9fb75ddda9e256dd0115bf33
tougshire <tougshire@tougshire.com>
2023-01-21 21:38:31 -0500

    add test_browser.py

---
+++ tests/test_browser.py
@@ -0,0 +1,61 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+# from selenium.webdriver.firefox.service import Service as FirefoxService
+# from webdriver_manager.firefox import GeckoDriverManager
+from django.contrib.auth import get_user_model
+
+from ..models import Technician
+
+class NewProjectWithNotesTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        print('23121k17')
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(10)
+
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_create_project_with_note(self):
+        self.user = get_user_model().objects.create_user(username='alpha', password='alpha', is_superuser=True)
+        self.tech = Technician.objects.create(user=self.user, name="Alpha")
+
+
+        self.selenium.get(self.live_server_url  + '/prosdib/project/create/')
+        el_username = self.selenium.find_element(By.ID, 'id_username')
+        el_password = self.selenium.find_element(By.ID, 'id_password')
+        el_form = self.selenium.find_elements(By.TAG_NAME, 'form')[0]
+        el_username.send_keys(self.user.username)
+        el_password.send_keys(self.user.username)
+
+        el_form.submit()
+
+        el_title = self.selenium.find_element(By.ID, 'id_title')
+        el_title.send_keys('Test Project')
+
+        el_addprojectnote = self.selenium.find_element(By.ID, 'button_addprojectnote')
+        el_addprojectnote.click()
+
+        el_projectnote_maintext = self.selenium.find_element(By.ID, 'id_projectnote_set-0-maintext')
+        el_projectnote_maintext.send_keys('First Test Note for Test Project')
+
+
+        el_form = self.selenium.find_elements(By.TAG_NAME, 'form')[0]
+        el_form.submit()
+
+
+
